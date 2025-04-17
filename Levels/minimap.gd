02e73e49e1df890084dbccba02cee89fb928fab9
Beth Cryer <52399621+beth-cryer@users.noreply.github.com>
2025-04-17 02:19:39 +0100

    feat: countdown and variable heartbeat
    heart rate is synced with the countdown speed. base rate is 60 so will align with real time until modified

--- Levels/minimap.gd
+++ Levels/minimap.gd
@@ -1,14 +1,11 @@
 extends SubViewport
 
-# Reference the camera and the player
-# "@export" keyword will expose these two variables in the inspector
 @export var camera_node : Node3D
 @export var player_node : Node3D
 
 var map_cam_position;
 
 func _process(delta: float) -> void:
-	# Let camera move with player
 	camera_node.set_global_position(map_cam_position.get_global_position())
 	camera_node.set_global_rotation(map_cam_position.get_global_rotation())
 
