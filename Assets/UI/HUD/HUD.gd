1e89fad6e5e4d6a97347a8ede97bf85e33392d15
danielday-dev <141579840+danielday-dev@users.noreply.github.com>
2025-04-20 11:31:40 +0100

    feat: HUD

--- Assets/UI/HUD/HUD.gd
+++ Assets/UI/HUD/HUD.gd
@@ -1,9 +1,1 @@
 extends Control
-
-
-func _on_inv_button_pressed() -> void:
-	$Inventory.visible = true
-
-func _on_inventory_mouse_exited() -> void:
-	#BUG yeah this doesnt work lmao
-	$Inventory.visible = false
