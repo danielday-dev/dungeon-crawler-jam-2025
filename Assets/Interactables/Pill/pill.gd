569d8f3276c417abf68ad4bd721485527f2316c8
jawdan-dev <34403286+jawdan-dev@users.noreply.github.com>
2025-04-20 16:58:09 +0100

    fix: me.

--- Assets/Interactables/Pill/pill.gd
+++ Assets/Interactables/Pill/pill.gd
@@ -1,6 +1,6 @@
 extends InteractableBase
 
-@export var m_item : Inventory.Item = Inventory.Item.;
+@export var m_item : Inventory.Item = Inventory.Item.NotAnItem;
 
 func interact_internal() -> void:
 	Player.s_instance.m_health = Player.s_instance.m_max_health;
