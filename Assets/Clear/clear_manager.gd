72546af937b179150eb768075b91c61badf62364
jawdan-dev <34403286+jawdan-dev@users.noreply.github.com>
2025-04-20 09:22:49 +0100

    fix: ClearNodes no longer required to be child of ClearManager.

--- Assets/Clear/clear_manager.gd
+++ Assets/Clear/clear_manager.gd
@@ -7,8 +7,6 @@ static var s_instance : ClearManager = null;
 
 @export var s_material : ShaderMaterial;
 
-#########################################################################
-
 var m_updated : bool = false;
 
 #########################################################################
@@ -20,28 +18,26 @@ func _init() -> void:
 	s_instance = self;
 	
 func _ready() -> void:
-	set_process(true);
-	
-	child_order_changed.connect(update_material);
-	
+	set_process(true);	
 	update_material();
 
-func _process(_delta: float) -> void:
+func _process(_delta : float) -> void:
 	if (!m_updated): return;
 	m_updated = false;
 	update_material();
 	
 #########################################################################
-	
+
 func update_material() -> void:
 	var clearPositions : Array[Vector3] = [];
 	var clearRange : Array[float] = [];
 	
-	for child : ClearNode in get_children():
-		if (child == null): continue;
-		clearPositions.append(child.global_position);
-		clearRange.append(child.m_clearRange);
-	
+	for node : ClearNode in get_tree().get_nodes_in_group("ClearNodes"):
+		if (clearPositions.size() >= 32): break;
+		if (node == null): continue;
+		clearPositions.append(node.global_position);
+		clearRange.append(node.m_clearRange);
+
 	s_material.set_shader_parameter("u_clearCenters", clearPositions);
 	s_material.set_shader_parameter("u_clearRange", clearRange);
 	s_material.set_shader_parameter("u_clearCount", min(32, clearPositions.size(), clearRange.size()));
--- Assets/Clear/clear_node.gd
+++ Assets/Clear/clear_node.gd
@@ -2,7 +2,6 @@
 extends Node3D
 class_name ClearNode;
 
-
 #########################################################################
 
 @export var m_clearRange : float:
@@ -19,7 +18,11 @@ func _ready():
 	m_lastPosition = global_position;
 	update_material();
 	set_process(true);
-	
+	add_to_group("ClearNodes");
+
+func _exit_tree() -> void:
+	update_material();
+
 func _physics_process(delta: float) -> void:
 	if (m_lastPosition == global_position): return;
 	m_lastPosition = global_position;
@@ -29,8 +32,6 @@ func _physics_process(delta: float) -> void:
 
 func update_material():
 	if (ClearManager.s_instance == null): return;
-	if (ClearManager.s_instance != get_parent()):
-		reparent(ClearManager.s_instance);
 	ClearManager.s_instance.m_updated = true;
 	
 #########################################################################
